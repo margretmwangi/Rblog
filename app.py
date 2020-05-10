24da3795f20dbc17b3f2aba3862233e13d492961
margaret mwangi <margaret13mwangi@gmail.com>
2020-05-10 19:35:20 +0300

    Add the final commit

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///Mblog.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
 
 db = SQLAlchemy(app)
 
