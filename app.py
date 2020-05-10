4fe9cd0ec08f074eb4376e481ff4ce0231996b3d
margaret mwangi <margaret13mwangi@gmail.com>
2020-05-10 18:27:15 +0300

    deployment to heroku

--- app.py
+++ app.py
@@ -1,6 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///Mblog.db'
+
+db = SQLAlchemy(app)
+
+
+class Blogpost(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(50))
+    subtitle = db.Column(db.String(50))
+    author = db.Column(db.String(20))
+    date_posted = db.Column(db.DateTime)
+    content = db.Column(db.Text)
 
 
 @app.route('/')
@@ -20,7 +34,22 @@ def post():
 
 @app.route('/add')
 def add():
-    return render_template('add.html')
+    return render_template('add .html')
+
+
+@app.route('/addpost', methods=['POST'])
+def addpost():
+    title = request.form['title']
+    subtitle = request.form['subtitle']
+    author = request.form['author']
+    content = request.form['content']
+
+    post = Blogpost(title=title, subtitle=subtitle, author=author, content=content, date_posted=datetime.now())
+
+    db.session.add(addpost)
+    db.session.commit()
+
+    return redirect(url_for('index'))
 
 
 if __name__ == '__main__':
